02ab96d6ddab9f6253d1a6618f8f227da185ccc3
Dislaik <datdislaik@gmail.com>
2020-09-09 23:57:27 -0300

    Update Config.cs

--- Client/Config/Config.cs
+++ Client/Config/Config.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CitizenFX.Core;
 
 namespace Outbreak
 {
     public class Config
     {
-        public bool Debug = false;
+        public Vector3 PlayerSpawn = new Vector3( 456.9026f, -991.0045f, 30.6895f );
         public int ZombieDamage = 20;
         public float DistanceZombieTargetToPlayer = 25.0f;
         public bool ZombieCanRun = false;
         public int PercentageVehiclesUndriveable = 90;
+        public bool Debug = false;
     }
 }
