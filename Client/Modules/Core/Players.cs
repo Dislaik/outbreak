c0280f14d127422ea54574d2e993c2e7e5b3acd9
Dislaik <datdislaik@gmail.com>
2020-09-09 23:58:40 -0300

    Rename files

---
+++ Client/Modules/Core/Players.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
+namespace Outbreak.Core
+{
+    class Players : BaseScript
+    {
+        Config Config = new Config();
+        public Players()
+        {
+            Exports["spawnmanager"].spawnPlayer(SpawnPosition());
+            Exports["spawnmanager"].setAutoSpawn(false);
+        }
+
+        public dynamic SpawnPosition()
+        {
+            dynamic obj = new ExpandoObject();
+            obj.x = Config.PlayerSpawn.X;
+            obj.y = Config.PlayerSpawn.Y;
+            obj.z = Config.PlayerSpawn.Z;
+            obj.heading = 0f;
+            obj.model = "s_m_y_marine_01";
+            return obj;
+        }
+    }
+}
