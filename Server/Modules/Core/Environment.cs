54ca82b6d92c201465c9b8a94abf37d4e9a21b89
Dislaik <datdislaik@gmail.com>
2020-09-09 23:54:11 -0300

    Create Environment.cs

---
+++ Server/Modules/Core/Environment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
+namespace server.Modules.Core
+{
+    class Environment : BaseScript
+    {
+        private Environment()
+        {
+            SetMapName("Los Santos and Blaine");
+            SetGameType("Zombie Roleplay");
+        }
+    }
+}
