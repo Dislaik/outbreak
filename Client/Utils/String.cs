56e83327d77d9fb8437a6712e0f24f693d84816e
Dislaik <datdislaik@gmail.com>
2020-09-20 17:49:16 -0300

    Added String Utils
    Empty LOL

---
+++ Client/Utils/String.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
+namespace Outbreak.Utils
+{
+    public class String
+    {
+
+    }
+}
+
+
