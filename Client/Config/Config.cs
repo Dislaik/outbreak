4c35d98d70573397537f524b3cc19077509345d9
Dislaik <datdislaik@gmail.com>
2020-09-20 17:51:54 -0300

    Update Config.cs

--- Client/Config/Config.cs
+++ Client/Config/Config.cs
@@ -12,9 +12,9 @@ namespace Outbreak
         public static dynamic PlayerSpawn = new { X = 456.9026f, Y = -991.0045f, Z = 30.6895f, Heading = 90.0f };
         public static int ZombieHealth = 250;
         public static int ZombieDamage = 20;
-        public static bool ZombieInstantDeathByHeadshot = true;
+        public static bool ZombieInstantDeathByHeadshot = false;
         public static bool ZombieCanRagdollByShots = false;
-        public static float DistanceZombieTargetToPlayer = 25.0f;
+        public static float ZombieDistanceTargetToPlayer = 25.0f;
         public static bool ZombieCanRun = false;
         public static int PercentageVehiclesUndriveable = 90;
         public static bool Debug = false;
