b933b9b98614e849f737689b780e07658e232627
Dislaik <datdislaik@gmail.com>
2020-11-02 06:08:15 -0300

    LOL i forgot remove this

--- Server/Modules/User/Example/Main.cs
+++ Server/Modules/User/Example/Main.cs
@@ -20,43 +20,11 @@ namespace Outbreak.User
             Command.Register("example", "Admin", new Action<Player, List<object>, string>((Source, Arguments, Raw) =>
             {
 
-                Source.TriggerEvent("Example:Test");
+                //Source.TriggerEvent("Example:Test");
 
 
             }), "Suggestion Example from Server-Side");
 
-            Command.Register("giveitem", "Admin", new Action<Player, List<object>, string>((Source, Arguments, Raw) =>
-            {
-                try
-                {
-                    if (Arguments.ToList().Count() < 1)
-                    {
-                        ChatMessage.Error(Source, "Missing arguments to define");
-                    }
-                    else if (Arguments.ToList().Count() < 3)
-                    {
-                        IPlayer.AddInventoryItem(Source, Arguments[0].ToString(), Convert.ToInt32(Arguments[1]));
-                        
-                    }
-                    else
-                    {
-                        Player Player = Players.AsEnumerable().ToList().FirstOrDefault(k => k.Handle == Arguments[2].ToString());
-                        if (Player == null)
-                        {
-                            ChatMessage.Error(Source, "Player ID not found");
-                        }
-                        else
-                        {
-                            IPlayer.AddInventoryItem(Player, Arguments[0].ToString(), Convert.ToInt32(Arguments[1]));
-                        }
-                    }
-                }
-                catch { }
-
-            }), "Add items to user inventory",
-            new { name = "Item", help = "Item to be added" },
-            new { name = "Amount", help = "Amount of items" },
-            new { name = "ID", help = "Player ID" });
         }
     }
 }
